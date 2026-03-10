7308f6a4b5270d84221bd428cb4b1535b2bf0879
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-03-10 14:37:39 -0400

    Move algorithms and types to turbocollatz_internal

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use std::env;
 use std::process;
-use std::collections::HashSet;
 use std::process::ExitCode;
-#[cfg(not(feature = "u64"))]
-type StepsType = u32;
-
-#[cfg(feature = "u64")]
-type StepsType = u64;
-
+use turbocollatz_internal::StepsType;
+use turbocollatz_internal::easy_collatz;
+use turbocollatz_internal::collatz_ranged;
 fn string_to_int(input: String) -> StepsType {
     match input.parse() {
         Ok(output) => {
-            return output;
+            output
         }
         Err(_) => {
             eprintln!("Bad arguments.");
@@ -20,43 +16,14 @@ fn string_to_int(input: String) -> StepsType {
     }
 }
 
-fn collatz(seed: StepsType, print_steps: bool, known_good: &HashSet<StepsType>, return_steps: bool) -> Vec<StepsType> {
-    let mut steps: Vec<StepsType> = vec!();
-    let mut x: StepsType = seed;
-
-    while !known_good.contains(&x) {
-        if x%2==0 {
-            x = x/2;
-        }
-        else {
-            x = 3*x+1;
-        }
-        if return_steps {steps.push(x)};
-
-        if print_steps {println!("{}", x);}
-    }
-    return steps;
-}
-
-fn collatz_ranged(start: StepsType, end: StepsType, use_slow: bool) -> u32 {
-    let mut known_good:HashSet<StepsType> = HashSet::from([1]);
-    let mut total_steps:u32 = 0;
-    for i in start..=end {
-        let steps:Vec<StepsType> = collatz(i, false, &known_good, true);
-        total_steps += steps.len() as u32;
-        if !use_slow {known_good.extend(steps);}
-    }
-    return total_steps;
-}
-
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        2 => {if string_to_int(args[1].clone())>0 {collatz(string_to_int(args[1].clone()), true, &HashSet::from([1]), false);} else {eprintln!("Bad arguments."); return ExitCode::from(64);}}
+        2 => {if string_to_int(args[1].clone())>0 {easy_collatz(string_to_int(args[1].clone()), true);} else {eprintln!("Bad arguments."); return ExitCode::from(64);}}
         3 => {if string_to_int(args[1].clone())>0 {println!("Finished in {} steps.", collatz_ranged(string_to_int(args[1].clone()), string_to_int(args[2].clone()), env::var("USE_SLOW").unwrap_or_default() == "true"))} else {eprintln!("Bad arguments."); return ExitCode::from(64);}},
         _ => {eprintln!("Bad arguments."); return ExitCode::from(64);}
     }
 
-    return ExitCode::SUCCESS;
+    ExitCode::SUCCESS
 }
